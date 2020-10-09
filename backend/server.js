bf169778952eec95fa5085ea1f70cc0e09186638
Justas Titovec <justastitovec@Justass-MacBook-Pro.local>
2020-10-09 17:22:38 +0300

    front end user authentication

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,21 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import colors from 'colors';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 connectDB();
 
 dotenv.config();
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Api is running');
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(
