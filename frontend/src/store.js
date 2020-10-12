fd2cdc44288a1a466ba07bc0d03fe79e2dcb77dd
Justas Titovec <justastitovec@Justass-MacBook-Pro.local>
2020-10-12 16:11:10 +0300

    added checkout-page 1

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -13,6 +13,8 @@ import {
   userUpdateProfileReducer
 } from './reducers/userReducers.js';
 
+import { orderCreateReducer } from './reducers/orderReducers.js';
+
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
@@ -20,7 +22,8 @@ const reducer = combineReducers({
   userLogin: userLoginReducer,
   userRegister: userRegisterReducer,
   userDetails: userDetailsReducer,
-  userUpdateProfile: userUpdateProfileReducer
+  userUpdateProfile: userUpdateProfileReducer,
+  orderCreate: orderCreateReducer
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
@@ -31,9 +34,14 @@ const userInfoFromStorage = localStorage.getItem('userInfo')
   ? JSON.parse(localStorage.getItem('userInfo'))
   : null;
 
+const shippingAddressFromStorage = localStorage.getItem('shippingAddress')
+  ? JSON.parse(localStorage.getItem('shippingAddress'))
+  : {};
+
 const initialState = {
   cart: {
-    cartItems: cartItemsFromStorage
+    cartItems: cartItemsFromStorage,
+    shippingAddress: shippingAddressFromStorage
   },
   userLogin: {
     userLogin: userInfoFromStorage
