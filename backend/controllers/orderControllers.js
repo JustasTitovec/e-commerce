081317cd20a7dfa42940b5bcfe0ca30d2c3db201
Justas Titovec <justastitovec@Justass-MacBook-Pro.local>
2020-10-15 17:47:18 +0300

    admin screen2

--- backend/controllers/orderControllers.js
+++ backend/controllers/orderControllers.js
@@ -82,6 +82,26 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc update order to delivered
+// @route GET /api/orders/:id/deliver
+// @access private/admin
+
+const updateOrderToDelivered = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isDelivered = true;
+    order.deriverAt = Date.now();
+
+    const updatedOrder = await order.save();
+
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error('Order not found');
+  }
+});
+
 // @desc get logged in user orders
 // @route GET /api/orders/myorders
 // @access private
@@ -91,4 +111,20 @@ const getMyOrders = asyncHandler(async (req, res) => {
   res.json(orders);
 });
 
-export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders };
+// @desc get all orders
+// @route GET /api/orders/
+// @access private/admin
+
+const getOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({}).populate('user', 'id name');
+  res.json(orders);
+});
+
+export {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
+  getMyOrders,
+  getOrders,
+  updateOrderToDelivered
+};
--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -2,19 +2,32 @@ import React, { useEffect, useState } from 'react';
 import { PayPalButton } from 'react-paypal-button-v2';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import { Row, Col, ListGroup, Image, Card } from 'react-bootstrap';
+import { Row, Col, ListGroup, Image, Card, Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message.js';
 import Loader from '../components/Loader';
-import { getOrderDetails, payOrder } from '../actions/orderActions.js';
-import { ORDER_PAY_RESET } from '../constants/orderConstants.js';
+import {
+  getOrderDetails,
+  payOrder,
+  deliverOrder
+} from '../actions/orderActions.js';
+import {
+  ORDER_PAY_RESET,
+  ORDER_DELIVER_RESET
+} from '../constants/orderConstants.js';
 
-const OrderScreen = ({ match }) => {
+const OrderScreen = ({ match, history }) => {
   const orderId = match.params.id;
   const dispatch = useDispatch();
 
   const orderPay = useSelector(state => state.orderPay);
   const { loading: loadingPay, success: successPay } = orderPay;
+
+  const orderDeliver = useSelector(state => state.orderDeliver);
+  const { loading: loadingDeliver, success: successDeliver } = orderDeliver;
+
+  const userLogin = useSelector(state => state.userLogin);
+  const { userInfo } = userLogin;
 
   const [sdkReady, setSdkReady] = useState(false);
 
@@ -33,6 +46,10 @@ const OrderScreen = ({ match }) => {
   }
 
   useEffect(() => {
+    if (!userInfo) {
+      history.pushState('/login');
+    }
+
     const addPaypalScript = async () => {
       const { data: clientId } = await axios.get('/api/config/paypal');
       const script = document.createElement('script');
@@ -46,9 +63,12 @@ const OrderScreen = ({ match }) => {
       document.body.appendChild(script);
     };
 
-    if (!order || successPay) {
+    if (!order || successPay || successDeliver) {
       dispatch({
         type: ORDER_PAY_RESET
+      });
+      dispatch({
+        type: ORDER_DELIVER_RESET
       });
       if (!order || order._id !== orderId) {
         dispatch(getOrderDetails(orderId));
@@ -60,10 +80,14 @@ const OrderScreen = ({ match }) => {
         setSdkReady(true);
       }
     }
-  }, [order, orderId, dispatch, successPay]);
+  }, [order, orderId, dispatch, successPay, successDeliver, userInfo, history]);
 
   const successPaymentHandler = paymentResult => {
     dispatch(payOrder(orderId, paymentResult));
+  };
+
+  const deliverHandler = order => {
+    dispatch(deliverOrder(order));
   };
 
   return loading ? (
@@ -95,7 +119,7 @@ const OrderScreen = ({ match }) => {
                 {order.shippingAddress.postalCode},
                 {order.shippingAddress.country}
               </p>
-              {order.isPaid ? (
+              {order.isDelivered ? (
                 <Message variant="success"></Message>
               ) : (
                 <Message variant="danger">Not delivered</Message>
@@ -191,6 +215,21 @@ const OrderScreen = ({ match }) => {
                   )}
                 </ListGroup.Item>
               )}
+              {loadingDeliver && <Loader />}
+              {userInfo &&
+                userInfo.isAdmin &&
+                order.isPaid &&
+                !order.isDelivered && (
+                  <ListGroup.Item>
+                    <Button
+                      type="button"
+                      className="btn btn-block"
+                      onClick={deliverHandler}
+                    >
+                      Mark as delivered
+                    </Button>
+                  </ListGroup.Item>
+                )}
             </ListGroup>
           </Card>
         </Col>
