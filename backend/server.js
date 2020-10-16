292de76dda70f6ed86124a7d51fc213be09caf0a
Justas Titovec <justas.titovec@yahoo.com>
2020-10-16 17:46:31 +0300

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -20,9 +20,20 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.get('/', (req, res) => {
-  res.send('Api is running');
-});
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('Api is running');
+  });
+}
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
@@ -32,9 +43,6 @@ app.use('/api/upload', uploadRoutes);
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
-
-const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 const PORT = process.env.PORT || 5000;
 app.listen(
---
+++ frontend/build/precache-manifest.8e8f34b3ebfb5949c3ad9e56670eb1f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bf4af142078da378edc183bb1e56633",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31bddfba868f6fd1881b",
+    "url": "/static/css/main.e97aba77.chunk.css"
+  },
+  {
+    "revision": "ef28fe8913a2f480696a",
+    "url": "/static/js/2.44466a97.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.44466a97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31bddfba868f6fd1881b",
+    "url": "/static/js/main.0c1c2969.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
